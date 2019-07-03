c0d84f34eedaef9b77762981db28f1f55b9ae9be
Rafael Oliveira <rafaelbco@gmail.com>
2019-07-03 11:58:31 -0300

    Preparing release 0.0.1

--- docs/HISTORY.rst
+++ docs/HISTORY.rst
@@ -1,7 +1,7 @@
 Changelog
 =========
 
-0.0.1 (unreleased)
+0.0.1 (2019-07-03)
 ------------------
 
 - First version.
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 import os
 
-version = '0.0.1.dev0'
+version = '0.0.1'
 
 setup(
     name='collective.zodbdebug',
